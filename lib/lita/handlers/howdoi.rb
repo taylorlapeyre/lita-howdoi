14257a3ee94983c73933b1717c5c267b80158720
Taylor Lapeyre <taylorlapeyre@gmail.com>
2015-03-05 10:08:29 -0600

    refactor

--- lib/lita/handlers/howdoi.rb
+++ lib/lita/handlers/howdoi.rb
@@ -3,60 +3,36 @@ require 'nokogiri'
 module Lita
   module Handlers
     class Howdoi < Handler
-      GOOGLE_SEARCH_URL = "http://stackoverflow.com/search"
+      SEARCH_URL = "http://stackoverflow.com/search"
 
       route(/^howdoi (.*)$/, :howdoi, command: true, help: {
         "howdoi QUERY" => "Searches Stack Overflow for the first answer it finds with a solution to QUERY.",
       })
 
-      def get_url(url, q="")
-        http.get(url, q: q).body
-      end
-
-      def is_question?(stackoverflow_url)
-        stackoverflow_url =~ %r(/questions/\d+/)
-      end
-
-      def get_google_links(args)
-        page = get_url(GOOGLE_SEARCH_URL, args)
+      def search_stack_overflow_for(search_query)
+        page = http.get(SEARCH_URL, q: search_query).body
         html = Nokogiri.HTML(page)
-        posts = []
-        html.css('.result-link a').each do |link|
-          posts << "http://stackoverflow.com#{link[:href]}"
-        end
-        posts
-      end
-
-      def get_link_at_pos(links, pos)
-        link = nil
-        pos.times do |i|
-          break if i > links.size
-          link = links[i]
-        end
-        link
+        html.css('.result-link a').map { |link| "http://stackoverflow.com#{link[:href]}" }
       end
 
       def howdoi(chat)
-        args = chat.matches[0].last.split.join("+")
-        links = get_google_links(args)
-        link = get_link_at_pos(links, 1)
+        search_query = chat.matches[0].last.split.join("+")
+        first_result = search_stack_overflow_for(search_query).first
 
-        if link
-          page = get_url link
-          html = Nokogiri.HTML(page)
-          ans = html.at_css(".answer")
+        if first_result
+          html = Nokogiri.HTML(http.get(first_result).body)
+          answer = html.at_css(".answer")
 
-          if ans
-            instruction = ans.css("pre").children.
+          if answer
+            instruction = answer.css("pre").children.
               collect(&:content).
               join(" " * 5 + '-' * 50 + "\n") ||
-              ans.at_css("code").content
+              answer.at_css("code").content
 
-            unless instruction.empty?
+            if !instruction.empty?
               chat.reply "```\n#{instruction}```"
             else
-              p ans.at_css('.post-text').content
-              chat.reply "```\n#{ans.at_css('.post-text').content}```"
+              chat.reply "```\n#{answer.at_css('.post-text').content}```"
             end
           end
         else
